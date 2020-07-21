a0f26cd1ed9e923bf83660500cd6b1983d980dc4
cnho <125925470@qq.com>
2020-07-21 16:21:05 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <footer-guide></footer-guide>
+  </div>
+</template>
+
+<script>
+import FooterGuide from "./components/FooterGuide/FooterGuide.vue";
+export default {
+  name: "App",
+  components: {
+    FooterGuide
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
